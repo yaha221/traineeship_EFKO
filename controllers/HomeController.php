6684204dd13bb3cbfb3cf5fd97b7fa0ea881968c
Yaha <toha.stepanyuck@gmail.com>
2022-11-02 21:29:23 +0300

    Перенесение обработки данных в контроллер

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -4,10 +4,21 @@ namespace app\controllers;
 
 use Yii;
 use yii\web\Controller;
+use app\models\Data;
 
 class HomeController extends Controller
 {
     public function actionIndex(){
-        return $this->render('index');
+        $request = Yii::$app->request;
+        $data = new Data;
+        if ($request->isPost) {
+            $monthPost = $request->post("month");
+            $tonnagePost = $request->post("tonnag");
+            $typePost = $request->post("type");
+            return $this->render('index',['monthPost' => $monthPost, 'tonnagePost' => $tonnagePost, 'typePost' => $typePost,'data' => $data]);
+        }
+        else {
+            return $this->render('index',['data' => $data]);
+        }
     }
 }
