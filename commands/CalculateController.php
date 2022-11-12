f82d400960ff9711e05d76ed45756b4888b864f8
Yaha <toha.stepanyuck@gmail.com>
2022-11-12 03:10:36 +0300

    Распределение кода по функция и добавление комментариев

--- commands/CalculateController.php
+++ commands/CalculateController.php
@@ -6,6 +6,14 @@ use yii\console\Controller;
 use yii\console\ExitCode;
 use app\models\Data;
 
+/**
+ * CalculateController команда работы приложения в консоли
+ * 
+ * @property string $month опция выбранного месяца
+ * @property string $tonnage опция выбранного тоннажа
+ * @property string $type опция выбранного типа
+ */
+
 class CalculateController extends Controller
 {
 
@@ -13,9 +21,19 @@ class CalculateController extends Controller
     public $type;
     public $tonnage;
 
+    /**
+     * Переопределение встроенной функции options 
+     * @param $actionID - the action id of the current request
+     * 
+     * @return string[]
+     */
     public function options($actionID)
     {
-        return['month', 'type', 'tonnage'];
+        return[
+            'month',
+            'type',
+            'tonnage'
+        ];
     }
 
     public function actionIndex()
@@ -24,37 +42,7 @@ class CalculateController extends Controller
         $typeKey=array_search($this->type,$data->types);
         $tonnageKey=array_search($this->tonnage,$data->tonnages);
         $monthKey=array_search($this->month,$data->months);
-        if (!$typeKey) {
-            echo "\033[31m Выполнение команды завершено с ошибкой" . PHP_EOL;
-            echo "\033[31m Не найден прайс для --type = " . $this->type . PHP_EOL;
-            echo "\033[31m Проверьте корректность введённых данных" . PHP_EOL;
-            return ExitCode::IOERR;
-        }
-        if (!$tonnageKey) {
-            echo "\033[31m Выполнение команды завершено с ошибкой" . PHP_EOL;
-            echo "\033[31m Не найден прайс для --tonnage = " . $this->tonnage . PHP_EOL;
-            echo "\033[31m Проверьте корректность введённых данных" . PHP_EOL;
-            return ExitCode::IOERR;
-        }
-        if (!$monthKey) {
-            echo "\033[31m Выполнение команды завершено с ошибкой" . PHP_EOL;
-            echo "\033[31m Не найден прайс для --month = " . $this->month . PHP_EOL;
-            echo "\033[31m Проверьте корректность введённых данных" . PHP_EOL;
-            return ExitCode::IOERR;
-        }
-        if ($this->month === null) {
-            echo "\033[31m Выполнение команды завершено с ошибкой" . PHP_EOL;
-            echo "\033[31m Необходимо ввести месяц" . PHP_EOL;
-            return ExitCode::IOERR;
-        }
-        if ($this->type === null) {
-            echo "\033[31m Выполнение команды завершено с ошибкой" . PHP_EOL;
-            echo "\033[31m Необходимо ввести тип" . PHP_EOL;
-            return ExitCode::IOERR;
-        }
-        if ($this->tonnage === null) {
-            echo "\033[31m Выполнение команды завершено с ошибкой" . PHP_EOL;
-            echo "\033[31m Необходимо ввести тоннаж" . PHP_EOL;
+        if(!$this->isAllOptionsInsert() || !$this->isAllItemFound($monthKey, $typeKey, $tonnageKey)) {
             return ExitCode::IOERR;
         }
         echo "\033[36m Калькулятор стоимости сырья" . PHP_EOL;
@@ -63,6 +51,75 @@ class CalculateController extends Controller
         echo "\033[1;33m Тип - " . $this->type . PHP_EOL;
         echo "\033[1;33m Тоннаж - " . $this->tonnage . PHP_EOL;
         echo "\033[32m Результат - " . $data->rated[$typeKey][$tonnageKey][$monthKey] . PHP_EOL;
+        $this->renderTable($data, $typeKey);
+        return ExitCode::OK;
+    }
+
+    /**
+     * Проверка на соответствие прайсу
+     * @param $monthKey - ключ месяца
+     * @param $typeKey - ключ типа
+     * @param $tonnageKey - ключ тоннажа
+     * 
+     * @return true|false
+     */
+    private function isAllItemFound($monthKey, $typeKey, $tonnageKey)
+    {
+        if (!$monthKey && $monthKey !== 0) {
+            echo "\033[31m Выполнение команды завершено с ошибкой" . PHP_EOL;
+            echo "\033[31m Не найден прайс для --month = " . $this->month . PHP_EOL;
+            echo "\033[31m Проверьте корректность введённых данных" . PHP_EOL;
+            return false;
+        }
+        if (!$typeKey) {
+            echo "\033[31m Выполнение команды завершено с ошибкой" . PHP_EOL;
+            echo "\033[31m Не найден прайс для --type = " . $this->type . PHP_EOL;
+            echo "\033[31m Проверьте корректность введённых данных" . PHP_EOL;
+            return false;
+        }
+        if (!$tonnageKey) {
+            echo "\033[31m Выполнение команды завершено с ошибкой" . PHP_EOL;
+            echo "\033[31m Не найден прайс для --tonnage = " . $this->tonnage . PHP_EOL;
+            echo "\033[31m Проверьте корректность введённых данных" . PHP_EOL;
+            return false;
+        }
+        return true;
+    }
+    
+    /**
+     * Проверка на ввод всех опций(значений)
+     * 
+     * @return true|false
+     */
+    private function isAllOptionsInsert()
+    {
+        if ($this->month === null) {
+            echo "\033[31m Выполнение команды завершено с ошибкой" . PHP_EOL;
+            echo "\033[31m Необходимо ввести месяц" . PHP_EOL;
+            return false;
+        }
+        if ($this->type === null) {
+            echo "\033[31m Выполнение команды завершено с ошибкой" . PHP_EOL;
+            echo "\033[31m Необходимо ввести тип" . PHP_EOL;
+            return false;
+        }
+        if ($this->tonnage === null) {
+            echo "\033[31m Выполнение команды завершено с ошибкой" . PHP_EOL;
+            echo "\033[31m Необходимо ввести тоннаж" . PHP_EOL;
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Отрисовка таблицы в консоли
+     * @param $data - прайс для отрисовки
+     * @param $typeKey - тип по которому будет отрисовываться таблица
+     * 
+     * @return null
+     */
+    private function renderTable($data, $typeKey)
+    {
         echo "\033[32m ╔═════╦════════╦═════════╦════════╦══════════╦═════════╦════════╗" . PHP_EOL;
         echo "\033[32m ║ м\т ║ Январь ║ Февраль ║ Август ║ Сентябрь ║ Октябрь ║ Ноябрь ║" . PHP_EOL;
         foreach($data->tonnages as $keyTonnage => $tonnageItem) {
@@ -84,6 +141,5 @@ class CalculateController extends Controller
             echo PHP_EOL;
         }
         echo "\033[32m ╚═════╩════════╩═════════╩════════╩══════════╩═════════╩════════╝" . PHP_EOL;
-        return ExitCode::OK;
     }
 }
