71104db387c99c96898025a3c05b2516ded16ef2
Yaha <toha.stepanyuck@gmail.com>
2022-11-27 23:27:38 +0300

    Убраны лишние методы

--- models/User.php
+++ models/User.php
@@ -2,90 +2,22 @@
 
 namespace app\models;
 
+use nkostadinov\user\models\User;
 use Yii;
-use yii\db\ActiveRecord;
-use yii\web\IdentityInterface;
 
-class User extends ActiveRecord implements IdentityInterface
+
+class MyUser extends User
 {
-    const ROLE_ADMIN = 40;
-    const ROLE_MODER = 30;
-    const ROLE_USER = 20;
-    const ROLE_DELETED = 0;
-
-    public static function tableName()
-    {
-        return 'user';
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public static function findIdentity($id)
-    {
-        return static::findOne(['id' => $id]);
-    }
-
-    public function rules()
+    public function attributeLabels()
     {
         return [
-            ['role', 'default', 'value' => self::ROLE_USER],
-            ['role', 'in', 'range' => [self::ROLE_ADMIN, self::ROLE_MODER, self::ROLE_USER, self::ROLE_DELETED]],
+            'username' => 'Имя пользователя',
+            'email' => 'Электронная почта',
+            'status' => 'Статус',
+            'created_at' => 'Дата создания',
+            'confirmed_on' => 'Дата потдверждения',
+            'updated_at' => 'Последнее посещение',
+            'register_ip' => 'Ip регистрации',
         ];
     }
-
-    /**
-     * {@inheritdoc}
-     */
-    public static function findIdentityByAccessToken($token, $type = null)
-    {
-
-    }
-
-    /**
-     * Finds user by username
-     *
-     * @param string $username
-     * @return static|null
-     */
-    public static function findByUsername($username)
-    {
-        return static::findOne(['username' => $username]);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getId()
-    {
-        return $this->getPrimaryKey();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getAuthKey()
-    {
-
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function validateAuthKey($authKey)
-    {
-
-    }
-
-    /**
-     * Validates password
-     *
-     * @param string $password password to validate
-     * @return bool if password provided is valid for current user
-     */
-    public function validatePassword($password)
-    {
-        return Yii::$app->security->validatePassword($password, $this->password);
-    }
-
 }
