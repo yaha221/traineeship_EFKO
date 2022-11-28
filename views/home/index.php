640363394f37a65fdc985791d65b7efcb2715ba8
Yaha <toha.stepanyuck@gmail.com>
2022-11-29 01:53:22 +0300

    Добавил alert-success на главную страницу с возможностью её отключения

--- views/home/index.php
+++ views/home/index.php
@@ -1,9 +1,27 @@
 <?php
     $this->title = 'Калькулятор стоимости поставки сырья';
     use yii\helpers\Html;
+    use yii\bootstrap\Alert;
     use yii\widgets\ActiveForm;
 
 ?>
+<?php
+    if (Yii::$app->user->isGuest === false && Yii::$app->user->identity->is_alert === 1) {
+        Alert::begin([
+            'options' => [
+                'class' => 'alert-success',
+                'id' => 'my-alert'
+            ],
+            'body' => "Здравствуйте, <b>" . Yii::$app->user->identity->username . "</b>, вы авторизовались в системе расчета стоимости доставки. 
+            Теперь все ваши расчеты будут сохранены для последующего просмотра в <a href=\"/home/history\" class=\"alert-link\"> журнале расчетов</a>. ",
+            'closeButton' => [
+                'id' => 'close-alert',
+            ],
+        ]);
+        Alert::end();
+    }
+?>
+
     <div class="row mt-5 p-5">
         <h1>Калькулятор стоимости поставки сырья</h1>
         <?php $form = ActiveForm::begin([
@@ -28,5 +46,4 @@
     <div class="row p-5">
     <div id="feedback"></div>
     </div>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
